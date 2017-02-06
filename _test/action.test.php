8c433e789107a62ae9e44d0ddecab1d8988954ee
leeyc0 <leeyc0@users.noreply.github.com>
2017-02-06 14:25:00 +0000

    refine unit test

--- _test/action.test.php
+++ _test/action.test.php
@@ -10,22 +10,31 @@
 class plugin_canonicalchinese_action_test extends DokuWikiTest {
    protected $pluginsEnabled = array('canonicalchinese');
 
-   public function test_canonicalchinese_replace() {
+   public function provider()
+   {
+     return array(
+       array(true,
+         '十萬個為甚麼<nochinesecanonical>真</nochinesecanonical>的是科學教育的好書 真眞<nochinesecanonical>科學教育</nochinesecanonical>的好書',
+         '十萬個爲甚麼<nochinesecanonical>真</nochinesecanonical>的是科學敎育的好書 眞眞<nochinesecanonical>科學教育</nochinesecanonical>的好書'
+       ),
+       array(false,
+         '十萬個為甚麼<nochinesecanonical>真</nochinesecanonical>的是科學教育的好書 真眞<nochinesecanonical>科學教育</nochinesecanonical>的好書',
+         '十萬個為甚麼<nochinesecanonical>真</nochinesecanonical>的是科學教育的好書 真眞<nochinesecanonical>科學教育</nochinesecanonical>的好書'
+       )
+     );
+   }
+   
+   /**
+    * @dataProvider provider
+    */
+   public function test_canonicalchinese_replace($enabled, $inputs_string, $expected_string) {
      global $conf;
-     $data[] = array("", '十萬個為甚麼<nochinesecanonical>真</nochinesecanonical>的是科學教育的好書 真眞<nochinesecanonical>科學教育</nochinesecanonical>的好書');
-     $expected_output_enabled = '十萬個爲甚麼<nochinesecanonical>真</nochinesecanonical>的是科學敎育的好書 眞眞<nochinesecanonical>科學教育</nochinesecanonical>的好書';
-     $expected_output_disabled = '十萬個為甚麼<nochinesecanonical>真</nochinesecanonical>的是科學教育的好書 真眞<nochinesecanonical>科學教育</nochinesecanonical>的好書';
+     $conf['plugin']['canonicalchinese']['enabled'] = $enabled;
+     $data[] = array("", $inputs_string );
      $event = new Doku_Event("IO_WIKIPAGE_WRITE", $data);
      $action_class = new action_plugin_canonicalchinese();
-
-     # test with conf disabled
      $action_class->replace_canonical_chinese($event);
-     $this->assertEquals($expected_output_disabled, $data[0][1]);
-
-     # test with conf enabled
-     $conf['plugin']['canonicalchinese']['enabled'] = true;
-     $action_class->replace_canonical_chinese($event);
-     $this->assertEquals($expected_output_enabled, $data[0][1]);
+     $this->assertEquals($expected_string, $data[0][1]);
   }
 }
 ?>
--- _test/syntax.test.php
+++ _test/syntax.test.php
@@ -10,7 +10,17 @@
 class plugin_canonicalchinese_syntax_test extends DokuWikiTest {
    protected $pluginsEnabled = array('canonicalchinese');
 
-   public function test_nocanonicalchinese_tag() {
+   public function provider()
+   {
+     return array(array(true),array(false));
+   }
+
+   /**
+    * @dataProvider provider
+    */
+   public function test_nocanonicalchinese_tag($enabled) {
+     global $conf;
+     $conf['plugin']['canonicalchinese']['enabled'] = $enabled;
      $info = array();
      $instructions = p_get_instructions('十萬個為甚麼<nochinesecanonical>真</nochinesecanonical>的是科學敎育的好書 真眞<nochinesecanonical>科學教育</nochinesecanonical>的好書');
      $expected_output = "\n<p>\n十萬個為甚麼真的是科學敎育的好書 真眞科學教育的好書\n</p>\n";
